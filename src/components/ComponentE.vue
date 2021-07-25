9b287854eec8d23f7de2dfa73fffaf812c25c4e2
Ming <ming.k@hotmail.com>
2021-07-25 10:45:12 +0800

    fix component e allow for dynamic rendering

--- src/components/ComponentE.vue
+++ src/components/ComponentE.vue
@@ -1,18 +1,45 @@
 <template>
-  <h2>Component E</h2>
-  <ComponentF />
+  <div
+    :style="{
+      backgroundColor: 'lightgrey',
+      border: '4px solid yellow',
+    }"
+  >
+    <h2 :style="{color: 'yellow'}">Component E</h2>
+    <span v-for="(block, index) in content.body" :key="index">
+      <component :is="block.component" :block="block"></component>
+    </span>
+  </div>
 </template>
 
 <script>
-import ComponentF from "./ComponentF.vue"
+import ComponentF from "./ComponentF.vue";
+import ComponentProxy from "./ProxyComponent.vue";
+import ComponentG from "./ComponentG.vue";
+import Foo from "./Foo.vue";
+import Bar from "./Bar.vue";
+import componentLayout from "./componentLayout.json";
+
 export default {
   name: "ComponentE",
+  data() {
+    return {
+      name: ComponentG,
+      blogTitle: "sometitle",
+      content: {
+        body: componentLayout,
+      },
+    };
+  },
   components: {
-    ComponentF
-  }
-}
+    ComponentF,
+    ComponentG,
+    ComponentProxy,
+    Foo,
+    Bar,
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
---
+++ src/components/ComponentG.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Heyy!!!!!!!!!! {{ block.name }} {{ block }}</div>
+</template>
+
+<script>
+import { h } from "vue";
+export default {
+  name: "componentg",
+  data() {
+    return {};
+  },
+  props: {
+    block: Object,
+  },
+  setup() {
+    return {
+      name: "Jenny",
+    };
+  },
+};
+</script>
